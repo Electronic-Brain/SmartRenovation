66f08d5671213e1b35e08e4115151ac227e94d07
Electronic-Brain <46529413+Electronic-Brain@users.noreply.github.com>
2019-09-13 12:30:35 +0600

    v1.0
    Initial release of SmartRenovation. New features will be added from community feedback. Mail us at support@electronicbrain.net if any help or support required for following plugin.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Smart-Renovation
+
+Smartly Update/Copy your important file(s)/Plugin(s) to your project from source.
+Email : support@electronicbrain.net 
+
+• **How to setup**
+Simply put 'Smart Renovation' folder in Editor folder in Unity.  
+
+# Contribution 
+This project is free to use. You can make changes and make it better. Your name will be added to Credit. But, Reproduction is strictly prohibated.
+
+# About SmartDLL 
+SmartDLL is a collection of neccessary managed plugin that we need to develop our desired project. But sometimes, some thing is so deep or complex that has never done before or rarely but not available for everyone. 
+Electronic Brain will develop those rare things from community's feedback. So your feedback and contribution is so helpful for us. Your donation will effect our development speed. We will be able to focus on Programming. Donation information has given below. 
+
+# About Electronic Brain 
+Electronic brain is a Game Studio based on Bangladesh. Currently, we have 2 Programmers and a Doctor who actually give us creative idea to work on. We're always looking forward to working with a Creative Person. Please notify us if you want to Join us,
+
+# Task List
+- [x] Initial Release 
+We will develop the following tasks. If any features need to be added, please mail to support@electronicbrain.net . 
+
+# Contributors 
+ >Srejon Khan
+ Game Programmer, Electronic Brain 
+ Using Unity over 4 years. Use C# and PHP,MySQL for backend. Worked on various client project and games. Worked as Assistant programmer in DR.Paedi and Lead Programmer in Last Breath.  
+
+ >Ashikur Rahman 
+ Game Programmer, Electronic Brain 
+ Using Unity over 3 years. Use C# and PHP,MySQL for backend.Worked on various client project and games. Successfull Lead Programmer in DR.Paedi and Assistant Programmer in Last Breath.  
+
+ #Support
+Donate us for developing any project without any trouble. We need more Programmers and Artists to work on various project and gifting something better to everyone. (All the money you donate is spent for Developing and Electronic Brain and not for any personal uses.)
+Support Us at Patreon: https://www.patreon.com/ElectronicBrain
+Youtube :https://www.youtube.com/electronicbrain/ 
+Website :http://electronicbrain.net/ 
+Facebook :https://facebook.com/electronicbrainpage
+Instagram:https://instagram.com/electronicbrainteam
+
+Have a Nice Day.
+
---
+++ Smart Renovation/SmartEditor.cs
@@ -0,0 +1,226 @@
+﻿/* This script may not be reproduced or selling anywhere without prior written permission of Electronic Brain.
+ * Project Developed by - 
+ * Srejon Khan 
+ * Game Programmer, Electronic Brain 
+ * 
+ * Ashikur Rahman 
+ * Game Programmer,Electronic Brain
+ * Email : support@electronicbrain.net 
+ */
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SmartEditor : EditorWindow
+{
+    public List<string> filePaths = new List<string>();
+    public List<string> folderPaths = new List<string>();
+    public string destinationPath;
+
+    [MenuItem("Electronic Brain/Smart Library/Smart Renovation")]
+    public static void ShowWindow()
+    {
+        GetWindow<SmartEditor>(false, "Smart Renovation", true);
+    }
+    void OnGUI()
+    {
+        #region Header
+        var headerStyle = new GUIStyle(GUI.skin.label)
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold
+        };
+        GUILayout.Label("Smart Renovation", headerStyle);
+        GUILayout.Label("Smartly Renovate your Plugins", headerStyle);
+        #endregion
+
+        #region Source Folders
+        GUILayout.Label("Source Folders", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical();
+        if (folderPaths != null)
+        {
+            for (int i = 0; i < folderPaths.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.TextField("Source Folder No." + (i + 1), folderPaths[i]);
+                if (GUILayout.Button("X", GUILayout.Width(20)))
+                {
+                    folderPaths.RemoveAt(i);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        EditorGUILayout.EndVertical();
+        if (GUILayout.Button("Add Source Folders"))
+        {
+            string path = EditorUtility.OpenFolderPanel("Select Folder", "", "");
+            if (path != null)
+            {
+                folderPaths.Add(path);
+            }
+        }
+        #endregion
+
+        #region Source Files
+        GUILayout.Label("Source Files", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical();
+        if (filePaths != null)
+        {
+            for (int i = 0; i < filePaths.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.TextField("Source File No." + (i + 1), filePaths[i]);
+                if (GUILayout.Button("X", GUILayout.Width(20)))
+                {
+                    filePaths.RemoveAt(i);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        EditorGUILayout.EndVertical();
+        if (GUILayout.Button("Add Source File"))
+        {
+            string path = EditorUtility.OpenFilePanel("Select File", "", "");
+            if (path != null)
+            {
+                filePaths.Add(path);
+            }
+        }
+        #endregion
+
+        #region Utilities
+        GUILayout.Label("Utilities", EditorStyles.boldLabel);
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save Source Paths"))
+        {
+            string pathsString = null;
+            for (int i = 0; i < filePaths.Count; i++)
+            {
+                pathsString += @filePaths[i] + ",";
+            }
+            pathsString += "•";
+            for (int i = 0; i < folderPaths.Count; i++)
+            {
+                pathsString += @folderPaths[i] + ",";
+            }
+            FilesPath filesPath = new FilesPath
+            {
+                paths = pathsString
+            };
+            string jsonData = JsonUtility.ToJson(filesPath);
+
+            string savePath = EditorUtility.SaveFilePanel("Save Current Path Data", "", "PathsData", "json");
+            System.IO.File.WriteAllText(savePath, jsonData);
+            this.ShowNotification(new GUIContent("Successfully Saved!"));
+        }
+        if (GUILayout.Button("Load Source Paths"))
+        {
+            string loadPath = EditorUtility.OpenFilePanel("Select File", "", "json");
+            string loadData = System.IO.File.ReadAllText(loadPath);
+
+            FilesPath filesPath = JsonUtility.FromJson<FilesPath>(loadData);
+
+            string[] parts = filesPath.paths.Split('•');
+            string[] splittedFilesPath = parts[0].Split(',');
+            string[] splittedFoldersPath = parts[1].Split(',');
+            filePaths.Clear();
+
+            for (int i = 0; i < splittedFilesPath.Length; i++)
+            {
+                if (i != splittedFilesPath.Length - 1) filePaths.Add(splittedFilesPath[i]);
+            }
+            for (int i = 0; i < splittedFoldersPath.Length; i++)
+            {
+                if (i != splittedFoldersPath.Length - 1) folderPaths.Add(splittedFoldersPath[i]);
+            }
+            this.ShowNotification(new GUIContent("Successfully Loaded!"));
+        }
+        EditorGUILayout.EndHorizontal();
+
+        var style = new GUIStyle(GUI.skin.button);
+        style.normal.textColor = Color.red;
+        if (GUILayout.Button("Clear All", style))
+        {
+            if (EditorUtility.DisplayDialog("Clear", "Are you sure you want to clear all selected files and folder?", "Clear All", "Do Not Clear"))
+            {
+                filePaths.Clear();
+                folderPaths.Clear();
+                this.ShowNotification(new GUIContent("Successfully Cleared!"));
+            }
+        }
+        #endregion
+
+        #region Destination
+        GUILayout.Label("Destination", EditorStyles.boldLabel);
+        EditorGUILayout.TextField("Destination Path", destinationPath);
+        if (GUILayout.Button("Select Destination Folder"))
+        {
+            string path = EditorUtility.OpenFolderPanel("Select Destination Folder", "", "");
+            if (path != null)
+            {
+                destinationPath = path;
+            }
+        }
+        #endregion
+
+        #region Actions
+        GUILayout.Label("Actions", EditorStyles.boldLabel);
+        if (GUILayout.Button("Update All"))
+        {
+            if (EditorUtility.DisplayDialog("Update All Files & Folders", "Are you sure you want to update all selected files and folder?", "Update All", "Do Not Update All"))
+            {
+                CopyFiles();
+                CopyFolders();
+                this.ShowNotification(new GUIContent("Successfully updated! Please refresh if needed."));
+            }
+        }
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Update Files Only"))
+        {
+            if (EditorUtility.DisplayDialog("Update All Files", "Are you sure you want to update all selected files?", "Update All", "Do Not Update All"))
+            {
+                CopyFiles();
+                this.ShowNotification(new GUIContent("Successfully updated! Please refresh if needed."));
+            }
+        }
+        if (GUILayout.Button("Update Folders Only"))
+        {
+            if (EditorUtility.DisplayDialog("Update All Folders", "Are you sure you want to update all selected Folders?", "Update All", "Do Not Update All"))
+            {
+                CopyFolders();
+                this.ShowNotification(new GUIContent("Successfully updated! Please refresh if needed."));
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        #endregion
+    }
+
+    void CopyFiles()
+    {
+        for (int i = 0; i < filePaths.Count; i++)
+        {
+            string path = System.IO.Path.GetDirectoryName(filePaths[i]);
+            System.IO.File.Copy(filePaths[i], destinationPath + "/" + System.IO.Path.GetFileName(filePaths[i]), true);
+        }
+        AssetDatabase.Refresh();
+    }
+    void CopyFolders()
+    {
+        for (int i = 0; i < folderPaths.Count; i++)
+        {
+            foreach (string dir in System.IO.Directory.GetDirectories(folderPaths[i], "*", System.IO.SearchOption.AllDirectories))
+            {
+                System.IO.Directory.CreateDirectory(dir.Replace(folderPaths[i], destinationPath + dir));
+            }
+            foreach (string newDir in System.IO.Directory.GetFiles(folderPaths[i], "*.*", System.IO.SearchOption.AllDirectories))
+            {
+                System.IO.File.Copy(newDir, newDir.Replace(folderPaths[i], destinationPath), true);
+            }
+        }
+        AssetDatabase.Refresh();
+    }
+    private class FilesPath
+    {
+        public string paths;
+    }
+}
